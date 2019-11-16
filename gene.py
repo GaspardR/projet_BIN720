a47b2f3e12925da86880fd66814bde49406373fc
gaspard <gaspard.reulet@usherbrooke.ca>
2019-11-16 12:45:51 -0500

    Added copy constructor method and a default sequence attribute for __init__, so the copy can return the original sequence

--- gene.py
+++ gene.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
 class Gene():
-    def __init__(self, gene_id, chromosome, start, end, strand):
+    def __init__(self, gene_id, chromosome, start, end, strand, sequence=''):
         self.gene_id = gene_id
         self.chromosome = str(chromosome)
         self.start = int(start)
         self.end = int(end)
         self.strand = strand
+        self.sequence = sequence
 
     def __len__(self):
         return self.end - self.start
+
+    def copy(self):
+        other = Gene(
+            self.gene_id,
+            self.chromosome,
+            self.start,
+            self.end,
+            self.strand,
+            self.sequence
+        )
+        return other
 
     def insert_sequence(self, pos, sequence):
         self.sequence = self.sequence[:pos] + sequence + self.sequence[pos:]
