dbd46d240eb74bb3adbf1e6937f5ab74ee92f981
gaspard <gaspard.reulet@usherbrooke.ca>
2019-11-18 16:57:11 -0500

    Added name and role attributes

--- gene.py
+++ gene.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 class Gene():
-    def __init__(self, gene_id, chromosome, start, end, strand, sequence=''):
+    def __init__(self, gene_id, name, chromosome, start, end, strand, sequence='', role=''):
         self.gene_id = gene_id
+        self.name = name
         self.chromosome = str(chromosome)
         self.start = int(start)
         self.end = int(end)
         self.strand = strand
         self.sequence = sequence
+        self.role = role
 
     def __len__(self):
         return self.end - self.start
@@ -24,18 +26,21 @@ class Gene():
     def copy(self):
         other = Gene(
             self.gene_id,
+            self.name,
             self.chromosome,
             self.start,
             self.end,
             self.strand,
             self.sequence
         )
+        other.role = self.role
         return other
 
-    def insert_sequence(self, pos, sequence):
-        self.sequence = self.sequence[:pos] + sequence + self.sequence[pos:]
-        self.end += len(sequence)
 
     def delete_sequence(self, start, end):
         self.sequence = self.sequence[:start] + self.sequence[end:]
         self.end -= end - start
+
+    def insert_sequence(self, pos, sequence):
+        self.sequence = self.sequence[:pos] + sequence + self.sequence[pos:]
+        self.end += len(sequence)
