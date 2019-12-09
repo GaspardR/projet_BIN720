b3eb229f816578b62369764e616b6216063420cd
gaspard <gaspard.reulet@usherbrooke.ca>
2019-12-09 17:05:25 -0500

    Added functionnality to class, with the insertion of genes and update of positions given to Chromosome instead of Cell

--- chromosome.py
+++ chromosome.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python3
 
+from bisect import bisect_right
+
+from gene import Gene
+
 class Chromosome():
-    def __init__(self, id, sequence):
+    def __init__(self, id, genes):
         self.id = id
-        self.sequence = sequence
-        self.start = 0
-        self.end = len(self.sequence)-1
+        self.genes = genes
+
+    def __len__(self):
+        return len(self.genes)
+
+    def copy(self):
+        return Chromosome(self.id, [g.copy() for g in self.genes])
+
+    def add_gene(self, gene, index=None):
+        if not isinstance(gene, Gene):
+            raise TypeError('gene argument passed was not a Gene()')
+        if not index:
+            index = bisect_right(self.genes, gene)
+        self.genes.insert(index, gene)
+
+    def remove_gene(self, index):
+        return self.genes.pop(index)
+
+    def update_positions(self, length, index):
+        while index < len(self.genes):
+            self.genes[index].start += length
+            self.genes[index].end += length
+            index += 1
